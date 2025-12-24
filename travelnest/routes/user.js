ab29dde285b15a4faba86ff88815c842b6bbcad9
DoppalapudiYeshwanth <yeshwanthd2006@gmail.com>
2025-12-24 12:24:23 +0530

    Cleared dependency issues

--- travelnest/routes/user.js
+++ travelnest/routes/user.js
@@ -2,16 +2,27 @@ const express = require("express");
 const router = express.Router();
 const WrapAsync = require("../utils/WrapAsync");
 const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup");
 });
 router.post("/signup",async(req,res)=>{
-    let {username , email,password} = req.body;
+    console.log("Post working");
+    console.log(req.body);
+    let {username, email,password} = req.body;
     let newUser = new User({username ,email});
-    let regUser = await User.register(newUser,password);
+    console.log(newUser);
+    let regUser = User.register(newUser,password);
     console.log(regUser);
     req.flash("addUser","Welcome to TravelNest");
+    res.redirect("/listings");
+});
+router.get("/login",(req,res)=>{
+    res.render("users/login");
+});
+router.post("/login",passport.authenticate("local",{failureRedirect : "/login",failureFlash :true,}),async(req,res)=>{
+    req.flash("create",("Welcome to TravelNest you are logged in."));
     res.redirect("/listings");
 });
 
